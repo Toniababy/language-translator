9208728ab5ca810e78dc522ef12da3fffe823cea
chizzysax09-stack <chizzysax09@gmail.com>
2025-12-19 21:17:50 +0100

    Add files via upload

---
+++ igbo_dic.py
@@ -0,0 +1,26 @@
+igbo_words = {
+    "Hello":"Ndewo",
+    "Thank you":"Daalu",
+    "Person":"Onye",
+    "House":"Ulo",
+    "Children":"Umu",
+    "Food":"Nri",
+    "Water":"Mmiri",
+    "Hand":"Aka",
+    "Head":"ise",
+    "Restaurant":"Ulo Oriri",
+    "Vegetable":"Akwukwo",
+    "Market":"Ulo ahia",
+    "Tooth":"Eze",
+    "Onion":"Yabasi",
+    "Leg":"Ukwu",
+    "Ear":"Nti",
+    "Aeroplane":"Ugboelu",
+    "Fish":"Azu",
+    "Rice":"Osikapa"
+}
+word = input("enter an igbo word: ")
+if word in igbo_words:
+    print(word, "means:", igbo_words[word])
+else:
+    print("word not found.")
